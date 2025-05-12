f73851f5b8e7b48aa30d62618e31e02d1f900da0
Jonathan William <jonathan.willian@embras.net>
2025-05-12 13:18:46 -0300

    v3

--- demandas/demandas.php
+++ demandas/demandas.php
@@ -1,3 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../conexao.php';
+
+$sql_demandas = "
+  SELECT
+  d.id,
+  d.titulo,
+  e.estagio,
+  date_format(d.data_abertura, '%d/%m/20%y') as data_abertura, 
+  u.nome as solicitante,
+  r.nome as responsavel
+  
+  from demandas d
+  left join estagios e on e.id = d.estagio
+  left join usuarios u on u.id = d.solicitante
+  left join usuarios r on r.id = d.atendente";
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -12,6 +31,10 @@
 
         <div class="menu-lateral">
             <h2>Sistema de demandas</h2>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
             <footer> V1.01 </footer>
         </div>
 
@@ -19,26 +42,37 @@
             <div class="pesquisa">
                 <div class="itens-pesquisa">
                     <form method="post">
-                        <input type="text" name="ref-demandas" placeholder="N° Demanda" style="padding: 5px 5px;">
-                        <button type="submit" name="pesquisar" style="padding: 5px 5px;"> Pesquisar </button>
+                        <input type="text" name="ref-demandas" placeholder="N° Demanda" style="padding: 5px 5px; background-color: #1e1e1e; border: 1px solid white; border-radius:5px">
+                        <button type="submit" name="pesquisar" style="padding: 5px 5px; background-color:rgb(4, 137, 189); border: 1px solid white; border-radius:5px; color:#1e1e1e"> Pesquisar </button>
                     </form>
                 </div>
             </div>
             <div class="lista-demandas">
+                <?php
+                $demandas = mysqli_query($mysqli,$sql_demandas);
+                ?>
                 <table>
                     <thead>
                         <th>Número</th>
                         <th>Data de abertura</th>
                         <th>Titulo</th>
                         <th>Estagio</th>
+                        <th>Solicitante</th>
                         <th>Responsavel</th>
                     </thead>
                     <tbody>
-                        <td>1</td>
-                        <td>09/05/2025</td>
-                        <td>Teste</td>
-                        <td>Em atendimento</td>
-                        <td>Jonathan</td>
+                         <?php
+                        while($dados_demandas = $demandas->fetch_assoc()){
+                        ?>
+                        <td><?php echo $dados_demandas['id']?></td>
+                        <td><?php echo $dados_demandas['data_abertura']?></td>
+                        <td><?php echo $dados_demandas['titulo']?></td>
+                        <td><?php echo $dados_demandas['estagio']?></td>
+                        <td><?php echo $dados_demandas['solicitante']?></td>
+                        <td><?php echo $dados_demandas['responsavel']?></td>
+                        <?php
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
